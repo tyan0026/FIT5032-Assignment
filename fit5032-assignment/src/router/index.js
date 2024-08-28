fcfd26d01121df3e2b48be47acb0f947819eb161
tyan0026 <tyan0026@student.monash.edu>
2024-08-28 10:41:07 +1000

    Fix routing

--- fit5032-assignment/src/router/index.js
+++ fit5032-assignment/src/router/index.js
@@ -1,19 +1,19 @@
-import {createRouter} from 'vue-router';
+import {createRouter, createWebHistory} from 'vue-router';
 import HomePage from '@/components/HomePage.vue';
-import RegiterForm from '@/components/RegiterForm.vue'; 
-// import LoginForm from '@/components/LoginForm.vue'; 
+import RegiterForm from '@/components/RegiterForm.vue';
+import LoginForm from '@/components/LoginForm.vue'; 
 
 
 const routes = [
     {path:'/', component: HomePage},
-    // {path:'/login', component: LoginForm}
+    {path:'/login', component: LoginForm},
     {path:'/register', component: RegiterForm}
 ]
 
 
 
 const router = createRouter({
-    // history: createWebHistory(process.env.BASE_URL),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes
 })
 
